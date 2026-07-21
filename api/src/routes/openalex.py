fd78fb7004e39ce5d2fd8fb7cfd6fcceea900f0e
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2026-07-21 15:32:15 -0300

    feat: added the remaining fields that was needed, fixed the semantic search to the normal search that is a elastic search so its faster

--- api/src/routes/openalex.py
+++ api/src/routes/openalex.py
@@ -7,22 +7,30 @@ openalex = Blueprint('openalex', __name__)
 def get_works():
     url = "https://api.openalex.org/works"
     campos_necessarios = [
-        "authorship",
-        "doi"
+        "authorships",
+        "title",
+        "publication_year",
+        "primary_location",
+        "doi",
+        "keywords",
     ]
     query_params = {
-        "search.semantic": "artifical intelligence",
+        "search": "artificial intelligence",
         "api_key": "MgJfPXsCK8MCzgYaud9PGp",
-        "select": ",".join(campos_necessarios)
+        "select": ",".join(campos_necessarios),
+        "filter": "has_abstract:true"
     }
     headers = {
         "User-Agent": "mailto:leonardobolfarini@gmail.com"
     }
     response = requests.get(url, params=query_params, headers=headers)
 
+
     if response.status_code == 200:
         dados = response.json()
+        resultados = dados['results']
+        authors = resultados['authorships']
         return jsonify(dados['results'])
 
     else:
-        return jsonify({ 'message': f'Erro na API: {response.text}' }, response.status_code)
+        return jsonify({ 'message': f'Erro na API: {response.text}' }), response.status_code
