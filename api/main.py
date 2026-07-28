2c73764978c8da257e7de8c3eb290cac6a666a57
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2026-07-28 18:01:35 -0300

    feat: returning the download url to the route so it can be used in the web soon

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 from src.routes.files import files_bp
 from src.routes.analytics import analytics_bp
-from src.routes.openalex import openalex
+from src.routes.openalex import openalex_bp
 from src.utils.clean_up import start_cleanup_thread
 
 FILE_TTL_SECONDS = 30 * 60
@@ -20,7 +20,7 @@ start_cleanup_thread(OUTPUT_FOLDER, FILE_TTL_SECONDS)
 
 app.register_blueprint(files_bp)
 app.register_blueprint(analytics_bp)
-app.register_blueprint(openalex, url_prefix='/openalex/')
+app.register_blueprint(openalex_bp, url_prefix='/openalex/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5009, debug=True)
