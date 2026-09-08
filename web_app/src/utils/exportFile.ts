ff5fc9d73397d2a99a77e625436938ca4650b022
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2026-09-08 18:45:08 -0300

    refactor: removed unused function

--- web_app/src/utils/exportFile.ts
+++ web_app/src/utils/exportFile.ts
@@ -1,5 +1,3 @@
-import { GraphEdgesFormat, GraphNodesFormat } from "../pages/types";
-
 export const exportToCSV = (
   data: { count: number; label: string }[],
   fileName: string,
@@ -28,43 +26,4 @@ export const exportToCSV = (
   link.click();
   document.body.removeChild(link);
   URL.revokeObjectURL(url);
-};
-
-export const exportToPajek = (
-  graphData: { nodes: GraphNodesFormat[]; edges: GraphEdgesFormat[] },
-  fileName: string,
-) => {
-  if (!graphData) return;
-
-  const nodeMap = new Map();
-
-  let verticesText = `*Vertices ${graphData.nodes.length}\n`;
-
-  graphData.nodes.forEach((node, index) => {
-    const id = index + 1;
-    const label = node.data.label;
-    nodeMap.set(label, id);
-    verticesText += `${id} "${label}"\n`;
-  });
-
-  let edgesText = `*Edges\n`;
-  graphData.edges.forEach((edge) => {
-    const sourceId = nodeMap.get(edge.data.source);
-    const targetId = nodeMap.get(edge.data.target);
-
-    if (sourceId && targetId) {
-      edgesText += `${sourceId} ${targetId} ${edge.data.weight}\n`;
-    }
-  });
-
-  const fullText = verticesText + edgesText;
-
-  const blob = new Blob([fullText], { type: "text/plain;charset=utf-8;" });
-  const url = URL.createObjectURL(blob);
-  const link = document.createElement("a");
-  link.setAttribute("href", url);
-  link.setAttribute("download", `${fileName}.net`);
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
 };
